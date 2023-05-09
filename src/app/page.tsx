98db5179851e99b0d3cb762649de29a9909fb852
nara04040 <nara04040@naver.com>
2023-05-09 16:16:11 +0900

    Style: page1에 대한 css설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Text Game repo입니다.</div>;
+  return (
+    <>
+      <div className="game-wrapper">
+        <div className="logo">logo</div>
+        <div className="game-desc">
+          <h2>게임 설명</h2>
+          <p>해리포터 캐릭터 찾기 게임에 오신 여러분! 환영합니다.</p>
+          <p>우리는 10가지의 테스트를 실시할 것이며 호그와트 캐릭터 중 하나를 배정받을 수 있습니다.</p>
+          <p>재밌는 경험이 되길 바라며 게임을 시작하고싶다면 Game Start버튼을 눌러주세요!</p>
+          <div className="start-button">
+            <Link href="/stage1">Game Start</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/app/stage1/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Stage1() {
+  return <h1>stage1입니다.</h1>;
+}
