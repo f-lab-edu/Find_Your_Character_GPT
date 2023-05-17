d2d72e46de94d5c31294cda428ce77e4aa22618f
nara04040 <nara04040@naver.com>
2023-05-17 13:06:24 +0900

    Fix: 새로고침시 style풀리는 이슈 해결

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: {
+      ssr: true,
+    },
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
