bf7db38c8af6c1f7dc3f20ba019c6e437954d1ad
Mangopapa1 <gustp26@gmail.com>
2023-05-19 01:48:09 +0900

    Style: margin 처리

--- src/components/floatButton/FloatButton.jsx
+++ src/components/floatButton/FloatButton.jsx
@@ -18,13 +18,13 @@ const FloatBtn = styled.button`
   background: #25809f;
   box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.5);
   cursor: pointer;
-  margin-right: 1em;
 
   &:hover {
     margin-left: 0px;
     transform: scale(1.1, 1.1);
     -ms-transform: scale(1.1, 1.1);
     -webkit-transform: scale(1.1, 1.1);
+    will-change: transform;
     box-shadow: 0px 5px 5px -2px rgba(0, 0, 0, 0.25);
   }
 `;
