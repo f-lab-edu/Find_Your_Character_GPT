c88868018d5cd929494a14e34a0a274a0c2f577f
Mangopapa1 <gustp26@gmail.com>
2023-05-23 13:46:58 +0900

    Fix: 수정사항 반영

--- src/components/contentFont/ContentFont.jsx
+++ src/components/contentFont/ContentFont.jsx
@@ -1,21 +1,17 @@
 import styled from "styled-components";
 
-export const ContentFont = ({ margin, size, desc }) => {
-  return (
-    <Content margin={margin} size={size}>
-      {desc}
-    </Content>
-  );
+export const ContentFont = ({ desc }) => {
+  return <Content>{desc}</Content>;
 };
 
 const Content = styled.p`
-  margin: ${({ margin }) => margin + "px"} 0;
+  margin: 30px 0;
   color: white;
   font-family: BookkMyungjo-Bd;
-  font-size: ${({ size }) => size + "px"};
+  font-size: 15px;
   line-height: 1.8;
 
   @media (max-width: 800px) {
-    font-size: ${({ size }) => size / 6 + "vw"};
+    font-size: 15 / 6 + "vw";
   }
 `;
