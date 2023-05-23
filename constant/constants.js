2768d0b3d854cad8cb72d26c68735d310653d690
nara04040 <nara04040@naver.com>
2023-05-23 17:20:59 +0900

    Setting: stage별 타이틀 constant로 변경

--- constant/constants.js
+++ constant/constants.js
@@ -1,2 +1,3 @@
 export const metaData = {};
 export const story = {};
+export const stageNumber = ["stage1", "stage2", "stage3", "stage4", "stage5", "stage6", "stage7", "stage8"];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { GameDescBox } from "@/components/GameDesc/GameDescBox";
 import { Logo } from "@/components/Logo/Logo";
+import { stageNumber } from "../../constant/constants";
 
 export default function Home() {
   return (
     <>
       <Logo />
-      <GameDescBox descHeader={"스테이지"} desc={"설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명"} startButtonDesc={"Start"} buttonDesc={""} />
+      <GameDescBox descHeader={stageNumber[0]} desc={"설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명"} startButtonDesc={"Start"} buttonDesc={""} />
     </>
   );
 }
