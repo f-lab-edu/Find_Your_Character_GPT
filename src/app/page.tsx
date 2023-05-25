0c40e947b1779e3c2278fb45cd7e478f81b5dd84
Mangopapa1 <gustp26@gmail.com>
2023-05-26 03:22:25 +0900

    Style: 불필요한 css 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { GameDescBox } from "@/components/GameDesc/GameDescBox";
-import { Logo } from "@/components/Logo/Logo";
+import { GameDescBox } from "@/components/gameDesc/GameDescBox";
 import { stageNumber } from "../../constant/constants";
 
 export default function Home() {
   return (
     <>
-      <Logo />
       <GameDescBox descHeader={stageNumber[0]} desc={"설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명"} startButtonDesc={"Start"} buttonDesc={""} />
     </>
   );
--- src/app/stage/page.jsx
+++ src/app/stage/page.jsx
@@ -1,15 +1,16 @@
 "use client";
 import React from "react";
-import { Logo } from "@/components/Logo/Logo";
-import { GameDescBox } from "@/components/GameDesc/GameDescBox";
+import { GameDescBox } from "@/components/gameDesc/GameDescBox";
 
 export default function StagePage() {
   return (
     <>
-      <div>
-        <Logo />
-        <GameDescBox descHeader={"스테이지"} desc={"설명설명설명"} startButtonDesc={""} buttonDesc={["싸운다", "도망간다", "멍때린다", "아몰랑"]} />
-      </div>
+      <GameDescBox
+        descHeader={"스테이지"}
+        desc={"설명설명설명"}
+        startButtonDesc={""}
+        buttonDesc={["싸운다", "도망간다", "멍때린다", "아몰랑"]}
+      />
     </>
   );
 }
