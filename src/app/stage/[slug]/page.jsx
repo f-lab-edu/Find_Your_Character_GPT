2b7d1622bbfb4f1f3d4a159556a4fd3aace47b5f
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2023-05-26 12:18:18 +0900

    Issue 16 스테이지 버튼 다이나믹 라우팅 처리 (#20)
    * Feat: dynamic routing 설정
    
    * Feat: stage페이지에서 버튼을 눌렀을 때 다음 스테이지로 이동
    
    * Feat: buttonDescription constant화 이후 stage별로 출력력

---
+++ src/app/stage/[slug]/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import { Logo } from "@/components/Logo/Logo";
+import { GameDescBox } from "@/components/GameDesc/GameDescBox";
+import { buttonDescription } from "../../../../constant/constants";
+
+export default function StagePage({ params }) {
+  const stageNumber = params.slug;
+
+  return (
+    <>
+      <div>
+        <Logo />
+        <GameDescBox descHeader={`스테이지${stageNumber}`} desc={"설명설명설명"} startButtonDesc={""} buttonDesc={buttonDescription[+stageNumber - 1]} stageNumber={stageNumber} />
+      </div>
+    </>
+  );
+}
