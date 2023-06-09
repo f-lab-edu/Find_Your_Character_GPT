035536cdc12c569bb9d125afdde36e0a37d7cbe2
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2023-06-09 15:37:55 +0900

    Issue 33 FloatButton의 handler함수 분리 (#38)
    * Refactor: clickHandlerGPT함수 부모컴포넌트로 Liting
    
    * Refactor: clickhandler를 GameDescBox로 올리기

--- src/components/GameDesc/GameDescBox.jsx
+++ src/components/GameDesc/GameDescBox.jsx
@@ -2,9 +2,47 @@ import { styled } from "styled-components";
 import { FloatButton } from "../floatButton/FloatButton";
 import { StartButton } from "../floatButton/StartButton";
 import { useState } from "react";
+import { atom, useRecoilState } from "recoil";
+
+const stageResultState = atom({
+  key: "stageResult",
+  default: [],
+});
 
 export const GameDescBox = ({ descHeader, desc, startButtonDesc, buttonDesc, stageNumber }) => {
   const [gptResult, setGptResult] = useState("");
+  const [stageResult, setStageResult] = useRecoilState(stageResultState);
+
+  async function clickHandlerGPT() {
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ value: stageResult.toString() }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      console.log(data.result);
+      setGptResult(data.result);
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  const clickHandler = (buttonDesc) => {
+    setStageResult([...stageResult, buttonDesc]);
+    console.log(stageResult);
+    if (stageNumber === "10") {
+      clickHandlerGPT();
+    }
+  };
 
   return (
     <>
@@ -14,7 +52,7 @@ export const GameDescBox = ({ descHeader, desc, startButtonDesc, buttonDesc, sta
         {!!startButtonDesc ? (
           <StartButton startButtonDesc={startButtonDesc} />
         ) : (
-          buttonDesc.map((ele, i) => <FloatButton buttonDesc={buttonDesc[i]} key={i} stageNumber={stageNumber} setGptResult={setGptResult} />)
+          buttonDesc.map((ele, i) => <FloatButton buttonDesc={buttonDesc[i]} key={i} stageNumber={stageNumber} clickHandler={clickHandler} />)
         )}
       </ButtonBox>
     </>
--- src/components/floatButton/FloatButton.jsx
+++ src/components/floatButton/FloatButton.jsx
@@ -1,50 +1,11 @@
 import styled from "styled-components";
 import { generateText } from "../../app/api/generate";
-import { atom, useRecoilState } from "recoil";
 import Link from "next/link";
 
-const stageResultState = atom({
-  key: "stageResult",
-  default: [],
-});
-
-export const FloatButton = ({ buttonDesc, stageNumber, setGptResult }) => {
-  const [stageResult, setStageResult] = useRecoilState(stageResultState);
-
-  async function clickHandlerGPT() {
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ value: stageResult.toString() }),
-      });
-
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-
-      console.log(data.result);
-      setGptResult(data.result);
-    } catch (error) {
-      console.error(error);
-      alert(error.message);
-    }
-  }
-
-  const clickHandler = () => {
-    setStageResult([...stageResult, buttonDesc]);
-    console.log(stageResult);
-    if (stageNumber === "10") {
-      clickHandlerGPT();
-    }
-  };
-
+export const FloatButton = ({ buttonDesc, stageNumber, clickHandler }) => {
   return (
     <Link href={stageNumber !== "10" ? `stage/${Number(stageNumber) + 1}` : `/result`}>
-      <FloatBtn onClick={clickHandler}>{buttonDesc}</FloatBtn>
+      <FloatBtn onClick={() => clickHandler(buttonDesc)}>{buttonDesc}</FloatBtn>
     </Link>
     // {/* disable해주기 useState를 활용하여 loading화면 띄우기 */}
   );
