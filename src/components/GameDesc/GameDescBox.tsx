834d4a662b9fcc208ff8b71629d1c983711cf483
Mangopapa1 <gustp26@gmail.com>
2023-06-28 05:30:33 +0900

    Fix: startDesc paragraph로 변환 및 변환의 따른 코드 변경

--- src/components/GameDesc/GameDescBox.tsx
+++ src/components/GameDesc/GameDescBox.tsx
@@ -21,13 +21,12 @@ type StageResult = {
 
 interface GameDescBoxProps {
   descHeader: string;
-  desc: string;
   startButtonDesc?: string;
   buttonDesc: [{ text: string; state: string }] | undefined;
   stageNumber: string;
 }
 
-export const GameDescBox = ({ descHeader, desc, startButtonDesc, buttonDesc, stageNumber }: GameDescBoxProps) => {
+export const GameDescBox = ({ descHeader, startButtonDesc, buttonDesc, stageNumber }: GameDescBoxProps) => {
   const [gptResult, setGptResult] = useRecoilState(gptResultState);
   const [stageResult, setStageResult] = useRecoilState<string[]>(stageResultState);
 
@@ -77,12 +76,14 @@ export const GameDescBox = ({ descHeader, desc, startButtonDesc, buttonDesc, sta
   return (
     <>
       <GlowText size={40} desc={descHeader} />
-      <Desc>{desc}</Desc>
+      <Desc><div>내가 만약 해리포터 영화 속 주인공이라면?</div>
+        <div>GPT가 찾아주는 나의 인생 마법사</div>
+        <p>🧙🏻‍♀️ 🧙🏻‍♀️ 🧙🏻‍♀️ 🧙🏻‍♀️ 🧙🏻‍♀️</p></Desc>
       <ButtonBox>
         {!!startButtonDesc ? (
           <StartButton startButtonDesc={startButtonDesc} />
         ) : (
-          buttonDesc.map((choice, i) => <FloatButton buttonDesc={choice.text} buttonIndex={i} key={i} stageNumber={stageNumber} clickHandler={clickHandler} buttonState={choice.state} />)
+          buttonDesc?.map((choice, i) => <FloatButton buttonDesc={choice.text} buttonIndex={i} key={i} stageNumber={stageNumber} clickHandler={clickHandler} buttonState={choice.state} />)
         )}
       </ButtonBox>
     </>
