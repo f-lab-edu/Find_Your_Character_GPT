e4b61c36151fa0f44ac67f1c056b160f35b944b7
nara04040 <nara04040@naver.com>
2023-06-27 20:15:17 +0900

    fix: change FloatButton styles

--- src/components/floatButton/FloatButton.tsx
+++ src/components/floatButton/FloatButton.tsx
@@ -22,19 +22,22 @@ export const FloatButton = ({ buttonDesc, stageNumber, clickHandler, buttonState
 };
 
 const FloatBtn = styled.button`
-  width: 130px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 50px;
+  padding: 40px;
   font-weight: 600;
-  font-size: 18px;
-  text-align: center;
-  line-height: 50px;
+  font-size: 24px;
   color: #fff;
-  border: none;
-  border-radius: 5px;
+  border-radius: 15px;
   transition: all 0.2s;
   background: #25809f;
   box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.5);
   cursor: pointer;
   margin-top: 1em;
+
   &:hover {
     margin-left: 0px;
     transform: scale(1.1, 1.1);
@@ -42,5 +45,10 @@ const FloatBtn = styled.button`
     -webkit-transform: scale(1.1, 1.1);
     will-change: transform;
     box-shadow: 0px 5px 5px -2px rgba(0, 0, 0, 0.25);
+  }
+  @media (max-width: 700px) {
+    height: 30px;
+    padding: 25px;
+    font-size: 16px;
   }
 `;
