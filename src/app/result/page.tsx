47ac7e2d29d9f5da7e24c25968a6db2e65b47c3d
Hyunse <92600646+Mangopapa1@users.noreply.github.com>
2023-07-09 05:44:56 +0900

    Issue 77 result 결과값 prompt수정 (#78)
    * Fix: script를 next/script로 변환
    
    * Feat: gptResult 결과 값 페이지에 반영
    
    * Feat: gpt프롬프트 변환

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -7,11 +7,23 @@ import { ImageBox } from "../../components/imageBox/ImageBox";
 import { ResultButton } from "../../components/floatButton/ResultButton";
 import { ShareModal } from "../../components/shareModal/ShareModal";
 import Link from "next/link";
+import { useRecoilValue } from "recoil";
+import { gptResultState } from "@/components/GameDesc/GameDescBox";
 
 interface ContentFontProp {
   size: number
 }
 export default function ResultPage() {
+  const gptResult = useRecoilValue(gptResultState);
+  console.log(gptResult);
+  // const result = JSON.parse(gptResult)
+
+
+  // const stringResult = JSON.stringify(gptResult);
+  // const encoded = encodeURIComponent(stringResult);
+  // console.log(encoded);
+
+
   const [modalOpen, setModalOpen] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   const handleDownload = useCallback(() => {
@@ -33,29 +45,19 @@ export default function ResultPage() {
         <MainImage><ImageBox size={250} name={"gryffindor"} /></MainImage>
         <GlowText
           margin={20}
-          desc={"용감하고 재기발랄한 모험가인 당신은"}
+          desc={gptResult.prefix}
           size={28}
         />
-        <GlowText desc="'해리포터'" size={40} margin={10} />
+        <GlowText desc={gptResult.name} size={40} margin={10} />
         <ContentFont size={17}>
-          {`해리는 시리즈 동안 아버지의 영구히 흐트러진 검은 머리와 어머니의 밝은
-            녹색 눈, 이마에 번개 모양의 흉터가 있는 것으로 묘사되고 있다. 그는 마른
-            얼굴과 혹 모양의 무릎으로 그의 나이에 비해 작고 마른것으로 묘사되며 뿔테
-            안경을 쓰고 있다. 첫 번째 책에서 그의 흉터는 해리가 자신의 외모에 대해
-            유일하게 좋아했던 것으로 묘사된다. 롤링은 해리의 벼락 같은 흉터 뒤에
-            숨겨진 의미를 묻는 질문에 해리가 겪은 일을 신체적으로 보여주고 싶었다고
-            말했다. 그것은 그가 안에서 겪어온 것에 대한 겉으로 드러난 표현이었다.
-            어떤 의미에서는 선택받은 사람이 되거나 저주받은 사람이 되는 것과
-            같습니다. 롤링은 또한 해리가 부모님의 잘생긴 외모를 물려받았다고 말했다.
-            시리즈의 후반부에서 해리는 키가 커지며, 일곱 번째 책은 아버지의 키를
-            '거의', 다른 캐릭터들에 의해 '키가 크다'라고 한다.`}
+          {gptResult.description}
         </ContentFont>
         <SimilarContainer>
           <SimilarBox>
             <ContentFont size={25}>어울리는 캐릭터</ContentFont>
             <GlowText
               margin={20}
-              desc={"론 위즐리"}
+              desc={gptResult.suitable}
               size={28}
             />
           </SimilarBox>
@@ -63,7 +65,7 @@ export default function ResultPage() {
             <ContentFont size={25}>안 어울리는 캐릭터</ContentFont>
             <GlowText
               margin={20}
-              desc={"볼드모트"}
+              desc={gptResult.unsuitable}
               size={28}
             />
           </SimilarBox>
