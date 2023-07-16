a23126d20e804ca01ecf3a1aeea7ea98b398d69f
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2023-07-17 00:32:44 +0900

    Fix: font설정이 안되는 문제 해결 (#86)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
 "use client";
 import "../styles/globals.css";
+import { Inter, Nanum_Myeongjo } from "next/font/google";
+import Script from "next/script";
 import { RecoilRoot } from "recoil";
+
+const inter = Inter({ subsets: ["latin"] });
+const nanumMyeongjo = Nanum_Myeongjo({ subsets: ["latin"], weight: ["400", "700", "800"] });
 
 import StyledComponentsRegistry from "./registry";
 import { styled } from "styled-components";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <head></head>
+    <html className={nanumMyeongjo.className}>
+      <head>
+        <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.2.0/kakao.min.js" integrity="sha384-x+WG2i7pOR+oWb6O5GV5f1KN2Ko6N7PTGPS7UlasYWNxZMKQA63Cj/B2lbUmUfuC" crossOrigin="anonymous"></Script>
+      </head>
       <body>
         <StyledComponentsRegistry>
           <RecoilRoot>
