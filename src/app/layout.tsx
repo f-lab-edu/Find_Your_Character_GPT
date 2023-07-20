45552c36b89894deeb6ed6e3d2ce46f993c80840
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2023-07-20 19:58:17 +0900

    Feat: Favicon추가 (#98)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html className={nanumMyeongjo.className}>
       <head>
         <title>Find My Character</title>
+        <link rel="icon" href="/img/harrypoterFavicon.png" />
         <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.2.0/kakao.min.js" integrity="sha384-x+WG2i7pOR+oWb6O5GV5f1KN2Ko6N7PTGPS7UlasYWNxZMKQA63Cj/B2lbUmUfuC" crossOrigin="anonymous"></Script>
       </head>
       <body>
