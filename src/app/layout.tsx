ba21d40e53390a25ba243c454bc352d2d58ef4c4
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2024-01-31 01:19:51 +0900

    Issue 138 유저피드백 kakao 공유하기 문제 (#139)
    * fix: 카카오 공유하기 에러 수정
    
    * feat: next/script 컴포넌트 사용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
 "use client";
 import "../styles/globals.css";
 import { Inter, Nanum_Myeongjo } from "next/font/google";
-import Script from "next/script";
 import { RecoilRoot } from "recoil";
 import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] });
 const nanumMyeongjo = Nanum_Myeongjo({ subsets: ["latin"], weight: ["400", "700", "800"] });
 
 import StyledComponentsRegistry from "./registry";
 import { styled } from "styled-components";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html className={nanumMyeongjo.className}>
       <head>
-        <title>Find My Character</title>
+        <title>Find Your Character</title>
         <meta property="og:title" content="Find your Harry poter chracter" />
         <meta property="og:description" content="당신과 어울리는 해리포터 캐릭터를 찾아드릴께요!" />
         <meta property="og:url" content="https://find-character-gpt.vercel.app" />
@@ -26,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap" rel="preload" />
-        <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.2.0/kakao.min.js" integrity="sha384-x+WG2i7pOR+oWb6O5GV5f1KN2Ko6N7PTGPS7UlasYWNxZMKQA63Cj/B2lbUmUfuC" crossOrigin="anonymous"></Script>
+        <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js" integrity="sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8" crossOrigin="anonymous" defer />
       </head>
       <body>
         <StyledComponentsRegistry>
